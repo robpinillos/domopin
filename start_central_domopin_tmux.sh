88284f68691a3e29306c826482bd17729646cd9c
robpinillos <robpinillos@gmail.com>
2019-07-14 19:20:12 +0200

    merge versions

---
+++ start_central_domopin_tmux.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SESSION=$USER
+
+tmux -2 new-session -d -s $SESSION
+# Setup a window for tailing log files
+tmux new-window -t $SESSION:0 -n 'roscore'
+tmux new-window -t $SESSION:1 -n 'central'
+tmux new-window -t $SESSION:2 -n 'rosbridge'
+
+
+#tmux select-window -t $SESSION:0
+#tmux split-window -v
+#tmux select-pane -t 0
+#tmux send-keys "roscore" C-m
+#tmux resize-pane -U 30
+#tmux select-pane -t 1
+#tmux send-keys "htop" C-m
+
+tmux select-window -t $SESSION:0
+#tmux send-keys "roscore" Enter
+tmux send-keys "roslaunch domopin_central multimaster.launch" Enter
+
+tmux select-window -t $SESSION:1
+#tmux send-keys "roscd roscd domopin_central" C-m
+tmux send-keys "sleep 8; roslaunch domopin_central domopin.launch" Enter
+
+tmux select-window -t $SESSION:2
+#tmux send-keys " sleep 8; roslaunch rosbridge_server rosbridge_websocket.launch" Enter
+
+
+# Set default window
+tmux select-window -t $SESSION:1
+
+# Attach to session
+tmux -2 attach-session -t $SESSION
+
+tmux setw -g mode-mouse off
+
+
+
